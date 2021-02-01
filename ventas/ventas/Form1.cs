4fe4953cd9245f15bcc2136e19e271ea055eabeb
moises orellana <moorellana@unah.hn>
2021-02-01 12:37:25 -0600

    agregando frase motivadora

--- ventas/ventas/Form1.cs
+++ ventas/ventas/Form1.cs
@@ -19,7 +19,7 @@ namespace ventas
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Hola Mundo");
+            MessageBox.Show("Piensa, Cree y Atrevete");
         }
     }
 }
